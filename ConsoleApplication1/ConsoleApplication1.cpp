a2f1f603c623548985ee759659dc7c0a8edce51c
lyx <doo8opnewman@outlook.com>
2024-08-07 20:36:18 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+typedef long long ll;
+
+// 声明 chu2 函数
+string chu2(string s);
+
+ll quick_pow(ll a, string b, ll c)
+{
+	ll ans = 1;
+	int nsize = b.size();
+	while (nsize != 0)
+	{
+		if ((b[nsize - 1] - '0') % 2 != 0)
+		{
+			ans = (ans * a) % c;
+		}
+		b = chu2(b);
+		nsize = b.size();
+		a = (a * a) % c;
+	}
+	return ans;
+}
+
+string chu2(string s)
+{
+	string ans;
+	int temp = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		temp = temp * 10 + s[i] - '0';
+		ans.push_back(temp / 2 + '0');
+		temp = temp % 2;
+	}
+	while (ans[0] == '0')
+	{
+		ans.erase(ans.begin());
+	}
+	return ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string b;
+	ll a, m;
+	cin >> a >> m;
+	cin >> b;
+	cout << quick_pow(a, b, m) << endl;
+}
---
+++ summer24.8.7/summer24.8.7.cpp
@@ -0,0 +1,94 @@
+﻿//#include<iostream>
+//using namespace std;
+//typedef long long ll;
+//int n;
+//ll a[100000], b[100000];
+//ll exdgcd(ll a, ll b, ll& x, ll& y)
+//{
+//	if (b == 0)
+//	{
+//		x = 1;
+//		y = 0;
+//		return a;
+//	}
+//	else
+//	{
+//		ll t = exdgcd(b, a % b, x, y);
+//		ll temp = x;
+//		x = y;
+//		y = temp - a / b * y;
+//		return t;
+//	}
+//}
+//ll CRT(ll* a, ll* b)
+//{
+//	ll m1 = a[0],r1=b[0];
+//	ll x, y;
+//	for (int i = 1; i < n; i++)
+//	{
+//		ll m2 = a[i], r2 = b[i];
+//		ll tempm = exdgcd(m1, m2, x, y);
+//		x = (x * (r2 - r1) / tempm);
+//		x = (x % (m2 / tempm) + m2 / tempm) % (m2 / tempm);
+//		r1 = (m1 * x + r1);
+//		m1 = (m1 * m2 / tempm);
+//	}
+//	return (r1 % m1 + m1) % m1;
+//}
+//int main()
+//{
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> a[i] >> b[i];
+//	}
+//	cout << CRT(a, b) << endl;
+//}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+//typedef __int128 ll;
+typedef long long ll;
+int n;
+ll a[100001], b[100001];
+ll exdgcd(ll a, ll b, ll& x, ll& y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	ll t = exdgcd(b, a % b, x, y);
+	ll temp = x;
+	x = y;
+	y = temp - a / b * y;
+	return t;
+}
+ll CRT(ll* a, ll* b)
+{
+	ll m1 = a[0], r1 = b[0];
+	ll x, y;
+	for (int i = 1; i < n; i++)
+	{
+		ll m2 = a[i], r2 = b[i];
+		ll tempm = exdgcd(m1, m2, x, y);
+		x = (x * (r2 - r1) / tempm);
+		x = (x % (m2 / tempm) + m2 / tempm) % (m2 / tempm);
+		r1 = (m1 * x + r1);
+		m1 = (m1 * m2 / tempm);
+	}
+	return (r1 % m1 + m1) % m1;
+}
+int main()
+{
+	scanf("%lld", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%lld%lld", &a[i], &b[i]);
+	}
+	printf("lld", CRT(a, b));
+	return 0;
+}
